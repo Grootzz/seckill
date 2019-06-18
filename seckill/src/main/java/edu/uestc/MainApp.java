1a7bd4f77239e34a5daf955eaaf41cf748e0019b
Grootzz <noodle.ax@gmail.com>
2019-06-18 08:56:24 +0800

    nope

--- seckill/src/main/java/edu/uestc/MainApp.java
+++ seckill/src/main/java/edu/uestc/MainApp.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Hello world!
+ * 商品秒杀系统
  */
 
 @SpringBootApplication
